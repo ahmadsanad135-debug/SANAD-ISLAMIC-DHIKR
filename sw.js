c09c9e77d1cbfcb095793e10ed34597c1cbca037
The legend Ahmad <ahmadsanad135@gmail.com>
2025-12-27 10:48:26 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,25 @@
+const cacheName = 'azkar-v4-final'; // تغيير الاسم لضمان التحديث
+const assets = [
+  './',
+  './index.html',
+  './dhikr-data.js', // تم تعديل الاسم هنا لضمان عمل الأوفلاين
+  './manifest.json',
+  './icon.png'
+];
+
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open('quran-v1').then(cache => cache.addAll(['index.html', 'quran.html'])));
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => {
+      return res || fetch(e.request);
+    })
+  );
 });
+ 
